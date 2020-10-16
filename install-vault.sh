e04693e23be653f432513de848bdd69e9701726a
Ikhsan Fajar Nugroho <59926991+ikhsannugs@users.noreply.github.com>
2020-10-16 23:10:09 +0700

    Update install-vault.sh

--- install-vault.sh
+++ install-vault.sh
@@ -5,6 +5,7 @@
 
 ### Install Vault ###
 wget https://releases.hashicorp.com/vault/1.5.4/vault_1.5.4_linux_amd64.zip
+apt-get install unzip
 unzip vault_1.5.4_linux_amd64.zip
 mv vault /usr/local/bin
 
